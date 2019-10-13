00074c6d7e99054283113975c8125f3994d2dc8e
Mark McPherson <msm1089@yahoo.co.uk>
2019-10-13 13:27:01 +0100

    added className to 1st link

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <a
+            className="App-link"
             href="https://devconnector.markmcpherson.co.uk"
             target="_blank"
             rel="noopener noreferrer"
