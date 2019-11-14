5eb86d44d6a55d79eb557e3631916db3846040e6
Mark McPherson <msm1089@yahoo.co.uk>
2019-11-14 15:22:06 +0000

    Altered layout

--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -7,26 +7,32 @@ class Homepage extends Component {
         <div className="App">
           <header className="App-header">
             <p>
-              <a
-                className="App-link"
-                href="https://devconnector.markmcpherson.co.uk"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                DevConnector
-              </a>
-              Social network site built with React and Redux and using JWT
-              authentication
+              <h3>
+                <a
+                  className="App-link"
+                  href="https://devconnector.markmcpherson.co.uk"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  DevConnector
+                </a>
+              </h3>
+              <p>
+                Social network site built with React and Redux and using JWT
+                authentication
+              </p>
             </p>
             <p>
-              <a
-                className="App-link"
-                href="https://hobnob-api.markmcpherson.co.uk"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Hobnob API
-              </a>
+              <h3>
+                <a
+                  className="App-link"
+                  href="https://hobnob-api.markmcpherson.co.uk"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Hobnob API
+                </a>
+              </h3>
               <p>
                 User directory API built with Node, Express for routing,
                 Elasticsearch for storage and Swagger for documentation.
@@ -34,7 +40,7 @@ class Homepage extends Component {
               <p>
                 I built this following the Test Driven Development paradigm. I
                 was surprised that for a relatively small API, it is over 1000
-                tests!!! Most of those are unit tests but also I have added
+                tests! Most of those are unit tests but also I have added
                 integration tests. Test tooling includes Cucumber, Mocha and
                 Istanbul.
               </p>
